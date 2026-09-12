e0d738eecda7ec509876b210d8593d12e50cbdfd
SelvaM <105146406+Selva780M@users.noreply.github.com>
2026-09-12 11:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -280,10 +280,7 @@ def fetch_one_stock(
                 extended_session=False,
             )
 
-            df = normalize_tv_data(
-                raw,
-                symbol
-            )
+            df = normalize_tv_data(raw,symbol)
 
             if (
                 df is None
