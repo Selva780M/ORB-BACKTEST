1f6b68bc6cab22fcc074fd9d735186c961d73a87
SelvaM <105146406+Selva780M@users.noreply.github.com>
2026-09-12 15:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ def fetch_one_stock(
             raw = tv.get_hist(
                 symbol=symbol,
                 exchange="NSE",
-                interval=Interval.in_5_minute,
+                interval=str(Interval.in_5_minute),
                 n_bars=int(n_bars),
                 fut_contract=None,
                 extended_session=False,
