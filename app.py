d06ac535f4a7e4cb46fdfbfc4bd2b447670ce560
SelvaM <105146406+Selva780M@users.noreply.github.com>
2026-09-12 15:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3892,3 +3892,17 @@ class TvDatafeed:
     def close(self):
 
         self.__close_connection()
+
+
+for symbol in ["SBIN", "ADANIPORTS", "APOLLOHOSP"]:
+    print("TEST:", symbol)
+
+    df = tv.get_hist(
+        symbol=symbol,
+        exchange="NSE",
+        interval="5",
+        n_bars=500,
+        extended_session=False
+    )
+
+    st.write(df.head() if df is not None else "NO DATA")
