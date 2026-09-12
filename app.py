4280d2c98bdebed9373ace0035e24046b239748c
SelvaM <105146406+Selva780M@users.noreply.github.com>
2026-09-12 15:48:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -866,19 +866,6 @@ def adx(
     )
 
 
-for symbol in ["SBIN", "ADANIPORTS", "APOLLOHOSP"]:
-    print("TEST:", symbol)
-
-    df = tv.get_hist(
-        symbol=symbol,
-        exchange="NSE",
-        interval="5",
-        n_bars=500,
-        extended_session=False
-    )
-
-st.write(df.head() if df is not None else "NO DATA")
-
 
 # ============================================================
 # SESSION VWAP
